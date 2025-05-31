00ca9f388403bdf013a2444151b8ae1119da240f
Suny02 <sunyk2002@gmail.com>
2025-06-01 02:03:30 +0530

    Update AllRoutes.jsx

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -4,17 +4,17 @@ import { Routes, Route } from "react-router-dom";
 import AllProducts from "../Pages/AllProducts";
 import Men from "../Pages/Men";
 import Women from "../Pages/Women";
-
 import DescriptionPage from "../components/Description/DescriptionPage";
 import AllshoesD from "../Pages/Shoes";
 import Cart from "../Pages/Cart";
 import WishList from "../Pages/WishList";
 import Login from "../Pages/Login";
-import Register from "../Pages/SignUp";
+import Signup from "../Pages/SignUp"; // <-- updated import
 import Checkout from "../Pages/Checkout";
 import Authentication from "../PrivateRoute/Authentication";
 import MyAccount from "../Pages/MyAccount";
 import AdminPage from "../Admin/AdminPage";
+
 const AllRoutes = () => {
   return (
     <div>
@@ -36,8 +36,7 @@ const AllRoutes = () => {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/myaccount" element={<MyAccount />} />
-
-        <Route path="/register" element={<Register />} />
+        <Route path="/signup" element={<Signup />} /> {/* <-- updated route */}
         <Route
           path="/checkout"
           element={
