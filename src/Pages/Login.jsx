94704df286c9370ff0969776e63f1a75e9f38a65
Suny02 <sunyk2002@gmail.com>
2025-05-31 21:36:33 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Button,
-  Checkbox,
   Flex,
   FormControl,
   FormLabel,
@@ -9,147 +8,121 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Link,
-  Spinner,
   Stack,
   Text,
   useToast,
 } from "@chakra-ui/react";
-//import swal from "sweetalert";
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
-import { login } from "../redux/AuthReducer/action";
-//import { LOGIN_S } from "../redux/AuthReducer/actionType";
-import Navbar from "../components/Navbar/Navbar";
 import { ViewIcon } from "@chakra-ui/icons";
-const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-  // const [username, setUsername] = useState("");
+import React, { useState } from "react";
+import { useNavigate, Link as RouterLink } from "react-router-dom";
+import axios from "axios";
+
+const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const toast = useToast();
-  const loading = useSelector((store) => store.AuthReducer.isLoading);
-  const pathRoute = location.state?.from?.pathname || "/";
   const [eye, setEye] = useState(false);
-  const handleEye = () => {
-    setEye((prev) => !prev);
-  };
-  const loginHandler = () => {
-    if (email === "nareshmewada014@gmail.com" && password === "naresh#111*") {
-      const params = {
-        email,
-        password,
-      };
-      dispatch(login(params, toast)).then(() => {
-        navigate("/admin", { replace: true });
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const handleEye = () => setEye((prev) => !prev);
+
+  const handleSignup = async () => {
+    if (!email || !password) {
+      toast({
+        title: "Fields cannot be empty",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
       });
-    } else if (email && password) {
-      const params = {
+      return;
+    }
+
+    try {
+      const res = await axios.post(`https://your-api-url/register`, {
         email,
         password,
-      };
-      dispatch(login(params, toast)).then((res) => {
-        if (res.payload.msg === "login successfully") {
-          toast({
-            title: "Login Success",
-            description: "You are successfully logged in",
-            status: "success",
-            duration: 3000,
-            isClosable: true,
-            position: "top",
-          });
-          navigate(pathRoute, { replace: true });
-        } else {
-          toast({
-            title: res.payload.msg,
-            status: "error",
-            duration: 3000,
-            isClosable: true,
-            position: "top",
-          });
-        }
+      });
+
+      if (res.data.msg === "Registration successful") {
+        toast({
+          title: "Signup Success",
+          description: "You can now log in",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+        navigate("/login");
+      } else {
+        toast({
+          title: res.data.msg || "Signup Failed",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+    } catch (error) {
+      toast({
+        title: "Signup Failed",
+        description: error.response?.data?.msg || error.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
       });
     }
   };
 
   return (
-    <>
-      <Flex minH={"100vh"} align={"center"} justify={"center"}>
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-          <Stack align={"center"}>
-            <Heading fontSize={"4xl"} textTransform={"uppercase"}>
-              Sign in to your account
-            </Heading>
-          </Stack>
-          <Box rounded={"lg"} boxShadow={"lg"} p={8}>
-            <Stack spacing={4}>
-              <FormControl id="username" isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input
-                  type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </FormControl>
-              <FormControl id="password" isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                  <Input
-                    type={eye ? "text" : "password"}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <InputRightElement h={"full"}>
-                    <Button variant={"ghost"} onClick={handleEye}>
-                      <ViewIcon />
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: "column", sm: "row" }}
-                  align={"start"}
-                  justify={"space-between"}
-                >
-                  <Checkbox>Remember me</Checkbox>
-                  <Link color={"blue.400"}>Forgot password?</Link>
-                </Stack>
-                <Button
-                  bg={"black"}
-                  color={"whitesmoke"}
-                  _hover={{
-                    bg: "none",
-                    color: "black",
-                    border: "1px solid black",
-                  }}
-                  onClick={loginHandler}
-                >
-                  {loading ? <Spinner /> : "Sign in"}
-                </Button>
-              </Stack>
-              <Stack pt={6}>
-                <Text align={"center"}>
-                  Don't have an account?
-                  <RouterLink to="/register" color={"blue.400"}>
-                    Signup
-                  </RouterLink>
-                </Text>
-              </Stack>
-            </Stack>
-          </Box>
+    <Flex minH="100vh" align="center" justify="center">
+      <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6}>
+        <Stack align="center">
+          <Heading fontSize="4xl">Create your account</Heading>
         </Stack>
-      </Flex>
-    </>
+        <Box rounded="lg" boxShadow="lg" p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={eye ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement h="full">
+                  <Button variant="ghost" onClick={handleEye}>
+                    <ViewIcon />
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Button
+              bg="black"
+              color="white"
+              _hover={{ bg: "gray.800" }}
+              onClick={handleSignup}
+            >
+              Sign up
+            </Button>
+            <Stack pt={6}>
+              <Text align="center">
+                Already a user?{" "}
+                <RouterLink to="/login" style={{ color: "blue" }}>
+                  Login
+                </RouterLink>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
 
-export default Login;
-
-// swal({
-//   text: "Login Success",
-//   icon: "success",
-// });
+export default Signup;
